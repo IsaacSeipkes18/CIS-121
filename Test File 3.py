c9315b1daa3f622aed8558d2aa905d1dbdde4e87
Isaac.Seipkes@mnsu.edu <Isaac.Seipkes@mnsu.edu>
2022-09-01 13:13:06 -0500

    Lab 2 part 4

--- Test File 3.py
+++ Test File 3.py
@@ -1,1 +1,4 @@
-
+#Horse years thing
+human_age = int(input("What is your age?"))
+horse_years = (3*(((((human_age)**2)-47)/7)+12))
+print("If you are",human_age,"years old, you would be",horse_years,"in horse years.")
---
+++ Test file 4.py
@@ -0,0 +1,5 @@
+# To calculate a humans height you do the following: age^2
+# Print out the humans age and estimated height
+age = float(input("How old are you?"))
+height = age**2
+print("If you are",age,"your are probably",height,"tall.")
