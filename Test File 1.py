fab25eac5ffcd29897b3cd21eb931840401a2695
Isaac.Seipkes@mnsu.edu <Isaac.Seipkes@mnsu.edu>
2022-09-01 12:49:49 -0500

    Start of lab 2

---
+++ Test File 1.py
@@ -0,0 +1,5 @@
+#asking user for age/converting input str to int
+human_age = int(input("How old are you?"))
+dog_year = 7
+human_in_dog_years = human_age*dog_year
+print("If you are",human_age,"years old","you would be",(human_in_dog_years),"in dog years")
---
+++ Test File 2.py
@@ -0,0 +1,4 @@
+first_name = input("What is your first name?")
+last_name = input("What is your last name?")
+age = input("How old are you?")
+print("Hello",first_name,last_name,"you're",age,"years old.")
